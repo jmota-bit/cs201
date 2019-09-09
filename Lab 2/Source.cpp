aeb8186e68bf78b7aeb8288951d17dc63ed33df5
Julio Mota <jcmota@alaska.edu>
2019-09-09 09:23:36 -0800

    More

---
+++ Lab 2/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main(int argc, char** argv)
+{
+	cout << "Please type an integer:\n";
+	int input;
+	cin >> input;
+
+	cout << (input + 1)*(input + 1);
+}
