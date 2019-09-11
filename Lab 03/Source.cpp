1ac947676ba14fbdc85a5451258ecb6c7e3900cc
Julio Mota <jcmota@alaska.edu>
2019-09-11 09:26:14 -0800

    Lab update

--- Lab 03/Source.cpp
+++ Lab 03/Source.cpp
@@ -3,14 +3,26 @@
 int main()
 {
 	int i = 0;
-	int n = 0;
+	int ln = 0;
+	int nu = 0;
 	
 	while (i >= 0 and i <= 20) {
 		std::cout << i << std::endl;
 		++i;
 	}
 
-	
-
+	while (ln >= 0) {
 		
+		while (nu <= 6) {
+			std::cout << "*";
+			++nu;
+			
+			if (nu > 6) {
+				break;
+			}
+		
+		}
+			
+			++ln;
+	}	
 }
