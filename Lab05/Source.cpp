f0a73be3265576f8de39c6ce91a6ad63d854d069
Julio Mota <jcmota@alaska.edu>
2019-09-13 10:09:38 -0800

    Lab 5

---
+++ Lab05/Source.cpp
@@ -0,0 +1,15 @@
+/*
+Julio Mota
+9/13/19
+CS201
+Lab #5
+
+This program takes a number that a user inputed and performs three different actions depending on the value of the input.
+*/
+
+#include <iostream>
+
+int main() 
+{
+	std::cout << "Print this";
+}
