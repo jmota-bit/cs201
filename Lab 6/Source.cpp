8e70b69001cf7c142475aa4d3dd959e92938f2c6
Julio Mota <jcmota@alaska.edu>
2019-09-16 10:16:56 -0800

    Main lab req. finished

--- Lab 6/Source.cpp
+++ Lab 6/Source.cpp
@@ -6,10 +6,14 @@ using std::endl;
 using std::string;
 
 void stringLength(string in) {
+	int len = in.size();
 
+	cout << "The length of this string is: " << len;
 }
 
 int main() {
+	cout << "Please enter a string (a word):\n";
+	
 	string word;
 	cin >> word;
 
