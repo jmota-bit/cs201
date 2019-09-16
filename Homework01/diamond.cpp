9de850f50b7f02c0ffe1c3661c2b84c425a0faf0
Julio Mota <jcmota@alaska.edu>
2019-09-15 19:17:12 -0800

    Start of Homework 1

---
+++ Homework01/diamond.cpp
@@ -0,0 +1,14 @@
+/*
+Julio Mota
+9/15/2019
+CS201
+Hmk #1
+
+This project performs two functions: one is to take an integer the user inputed and uses it to draw a diamond made of "#" of the specified size; second is to convert mph to kph.
+*/
+
+#include <iostream>
+
+int main() {
+	std::cout << "Compile";
+}
