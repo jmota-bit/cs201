cc67174e63963bffdf92f5477a27e1e1375d5bf1
Julio Mota <jcmota@alaska.edu>
2019-09-15 20:53:48 -0800

    Algorithm start

--- Homework01/diamond.cpp
+++ Homework01/diamond.cpp
@@ -10,11 +10,28 @@ This project performs two functions: one is to take an integer the user inputed 
 #include <iostream>
 using std::cout;
 using std::cin;
+using std::endl;
+
+//This function takes the user input and draws the diamond.
+void diamond(int in) {
+	for (int line = 0; line < in; ++line) {
+		
+		for (int dot = 1; dot <= (line * -1 + in - 1); dot++) {
+			cout << " ";
+		}
+			cout << "#" << endl;
+
+		
+	}
+	
+}
 
 int main() {
-	cout << "This program makes a diamond sized off of the integer inputed. Please enter a positive integer." << std::endl;
+	cout << "This program makes a diamond sized off of the integer inputed. Please enter a positive integer." << endl;
 	
 	int input;
 	cin >> input;
-	
+	diamond(input);
+
 }
+
