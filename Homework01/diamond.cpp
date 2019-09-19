22e669e6dd134e0afd86e9fd7b6f262afbfd76a9
Julio Mota <jcmota@alaska.edu>
2019-09-19 11:45:06 -0800

    Start of Homework 2

--- Homework01/diamond.cpp
+++ Homework01/diamond.cpp
@@ -6,24 +6,12 @@ Hmk #1
 
 This project performs two functions: one is to take an integer the user inputed and uses it to draw a diamond made of "#" of the specified size; second is to convert mph to kph.
 */
-
 #include <iostream>
+
 
 using std::cout;
 using std::cin;
 using std::endl;
-
-//This function takes the user input and draws the diamond.
-void diamond(int in) {
-	for (int line = 0; line <= in; ++line) {
-
-		for (int dot = 1; dot <= (line * -1 + in - 1); dot++) {
-			cout << " ";
-		}
-		
-		cout << "#" << endl;
-	}
-}
 
 //This function converts miles to kilometers.
 void Conv(int miles) {
@@ -34,6 +22,17 @@ void Conv(int miles) {
 	cout << miles << " miles = " << kilom << " kilometers" << endl;
 }
 
+//This function produces the "diamond".
+void diamond(int in) {
+	for (int line = 0; line <= in; ++line) {
+
+		for (int dot = 1; dot <= (line * -1 + in - 1); dot++) {
+			cout << " ";
+		}
+
+		cout << "#" << endl;
+	}
+}
 
 int main() {
 	cout << "This program makes a diamond sized off of the integer inputed. Please enter a positive integer." << endl;
