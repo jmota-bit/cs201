caef3574002775c0621c5737e3ca51f292764e39
Julio Mota <jcmota@alaska.edu>
2019-09-22 16:32:14 -0800

    Boolean function

--- hmk2/intio.h
+++ hmk2/intio.h
@@ -1,8 +1,14 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
 //This function creates a vector called "names", then prompts the user to enter a name, which is put into a string object "name" using the standard library function getline();. That object then gets placed into each index value.
-void InputNames() {
+void InputNames() 
+{
 	vector<string> names;
 	for (int i = 0; i < 10; i++) {
 		string name;
@@ -10,10 +16,23 @@ void InputNames() {
 		getline(cin, name);
 		names.push_back(name);
 	}
+
 }
 
 //This function checks whether a specified name is in the vector. If it exists, it returns "'name' does exist". If it doesn't exist, it returns "'name' does NOT exist".
-bool DoesNameExist(const string nameTofind, const vector<string>& names)
+bool DoesNameExist(const string & nameTofind, const vector<string> & names)
 {
-	
+	for (int i = 0; i < names.size(); i++)
+	{
+		if (nameTofind == names[i])
+		{
+			cout << nameTofind << " does exist.";
+			return true;
+		}
+		if (i == (names.size() - 1) && nameTofind != names[i])
+		{
+			cout << nameTofind << " does NOT exist.";
+			return false;
+		}
+	}
 }
--- hmk2/main.cpp
+++ hmk2/main.cpp
@@ -7,13 +7,11 @@
 using namespace std;
 
 int main(int argc, char** argv)
-{
+{	
+	
 	InputNames();
 	
-	bool DoesNameExist(const string nameTofind, const vector<string> & names);
-
-	void PrintNames(const vector<string> & names);
-
+	bool DoesNameExist(const string & nameTofind, const vector<string> & names);
 	
 	return 0;
 }
