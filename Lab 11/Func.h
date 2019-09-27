b2870c499abc69ed970b9205722621991b860dd6
Julio Mota <jcmota@alaska.edu>
2019-09-27 10:21:21 -0800

    Lab 11

---
+++ Lab 11/Func.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+string lowerCase(string word);
+
---
+++ Lab 11/lowerCaseMain.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+#include "Func.h"
+
+using namespace std;
+
+int main()
+{
+	
+	string input;
+	getline(cin, input);
+
+	vector<char> word;
+	for (int i = 0; i < word; i++)
+	{
+		word[i] = input;
+	}
+
+	return 0;
+}
